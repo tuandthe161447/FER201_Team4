efdbe1447e75ac3fef2a17ee8e84cac063402758
pmt1506 <pmtien150603@gmail.com>
2023-07-12 03:55:53 +0700

    add design for posts

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/header';
 import Slide_hearder from './components/slide_header';
 import Footer from './components/footer';
+import Posts from './components/posts';
 
 function App() {
   return (
     <div className='container-fluid' >
       <BrowserRouter>
-      <Header/>
-      <Slide_hearder/>
-      <Footer/>
+      {/* <Header/> */}
+      {/* <Slide_hearder/> */}
+      <Routes>
+        <Route path='/posts' element={<Posts/>}/>
+      </Routes>
+      {/* <Footer/> */}
       </BrowserRouter>
 
     </div>
--- src/components/header.js
+++ src/components/header.js
@@ -13,7 +13,7 @@ const Header = () => {
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="mr-auto">
-                            <Nav.Link href="#features">Blogs</Nav.Link>
+                            <Nav.Link href="/posts">Posts</Nav.Link>
                             <NavDropdown title="Categories" id="collasible-nav-dropdown">
                                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
